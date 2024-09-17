95ac48dfc5c5d74b76fe390c1d7be1495caa5ce1
Jeremy Packard <wolfpack.adventurer@icloud.com>
2024-09-17 13:04:51 -0400

    fixed allowed hosts

--- budget_app/budget_app/settings.py
+++ budget_app/budget_app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-ipb+8b2slj%irv8t*0sal#(#8jzitblb%a=%*au+_+am8al=pm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
